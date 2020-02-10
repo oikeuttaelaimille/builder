e554482cbd71c412f792b1a17f638e1a2f4c6351
Santeri Hurnanen <santeri@oikeuttaelaimille.fi>
2020-02-10 21:37:24 +0200

    🐍 🐸 🍀 🦀 🐲

--- index.js
+++ index.js
@@ -8,10 +8,12 @@ const app = require('./src/app')
 const http = require('http')
 
 const DEFAULT_PORT = 3000
+const DEFAULT_HOST = 'localhost'
 
-const exec = process.argv.slice(0, 2)
+// const exec = process.argv.slice(0, 2)
 const args = process.argv.splice(process.execArgv.length + 2)
 
-const listenTo = args[0] === '-' ? process.stdin : args[0] || DEFAULT_PORT
+const port = args[0] || DEFAULT_PORT
+const host = args[1] || DEFAULT_HOST
 
-http.createServer(app).listen(listenTo, () => console.log(`Listening to ${args[0] || DEFAULT_PORT}`))
+http.createServer(app).listen(port, host, () => console.log(`Listening to ${host}:${port}`))
--- test.sh
+++ test.sh
@@ -1,26 +1,10 @@
 #!/usr/bin/env bash
 
-function finish {
-  echo 'Build failed :('
-  exit 1
-}
-
-# If a command fails, make the whole script exit.
-set -eEuf -o pipefail
-trap finish ERR
-
 echo "starting build $@"
 
-# cd ~/Private/oikeuttaelaimille/frontend
-cd /var/www/oikeuttaelaimille/frontend
-
-export GATSBY_DRUPAL_HOST='http://localhost:8888'
-export DRUPAL_USERNAME='gatsby'
-export DRUPAL_PASSWORD='test-api-key'
-
-# Turn on better format for loggin on Gatsby commands:
-export CI='true'
-
-npx gatsby build
+for i in {1..10}; do
+  date
+  sleep 1
+done
 
 echo 'end'
